dbb9a1c8b3cbc85632072056843020c2e77ad01a
ankit mittal <ankit05.developer@gmail.com>
2025-04-15 19:17:35 +0530

    changes in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1>Github Actions</h1>
+      <h1>Github Actions Project</h1>
       <h2>Complete course with 2 projects</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
